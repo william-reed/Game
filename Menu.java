1e20cd9a51e9eebab040d0975570d4f9191cb486
Derp cherp <wreed58@gmail.com>
2012-10-21 16:12:49 -0400

    added game files

---
+++ Menu.java
@@ -0,0 +1,42 @@
+
+
+import org.lwjgl.input.Mouse;
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+public class Menu extends BasicGameState{
+   
+   Image background;
+   
+   public Menu(int state){
+   }
+   
+   public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
+      background = new Image("res/menuImage.png");
+   }
+   
+   public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
+      background.draw(0,0);
+   }
+   
+   public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
+      int posX = Mouse.getX();
+      int posY = Mouse.getY();
+      //play button
+      if((posX>439 && posX<561)&&(posY>282 && posY<304)){
+         if(Mouse.isButtonDown(0)){
+            sbg.enterState(1);
+         }
+      }
+      //exit game
+      if((posX>452 && posX<547)&&(posY>216 && posY<241)){
+         if(Mouse.isButtonDown(0)){
+            System.exit(0);
+         }
+      }
+   }
+   
+   public int getID(){
+      return 0;
+   }
+}
---
+++ Play.java
@@ -0,0 +1,171 @@
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+public class Play extends BasicGameState {
+
+	Animation bucky, movingUp, movingDown, movingLeft, movingRight; // 4
+																	// animations,
+																	// bucky
+																	// will be
+																	// set to
+																	// one
+	Image worldMap, menu, sprite1;
+	boolean quit = false;
+	int[] duration = { 200, 200 }; // duration or length of the frame
+	float buckyPositionX = 0; // bucky will start at coordinates 0,0
+	float buckyPositionY = 0;
+	float shiftX = buckyPositionX + 500; // this will shift the screen so bucky
+											// appears in middle
+	float shiftY = buckyPositionY + 281; // half the length and half the width
+											// of the screen
+	int spriteone;
+	float spritex = 675;
+	float spritey = 743;
+
+	public Play(int state) {
+	}
+
+	public void init(GameContainer gc, StateBasedGame sbg)
+			throws SlickException {
+		worldMap = new Image("res/world.png");
+		menu = new Image("res/menu.png");
+		sprite1 = new Image("res/sprite1.png");
+		Image[] walkUp = { new Image("res/buckysBack.png"),
+				new Image("res/buckysBack.png") }; // these are the images to be
+													// used in the "walkUp"
+													// animation
+		Image[] walkDown = { new Image("res/buckysFront.png"),
+				new Image("res/buckysFront.png") };
+		Image[] walkLeft = { new Image("res/buckysLeft.png"),
+				new Image("res/buckysLeft.png") };
+		Image[] walkRight = { new Image("res/buckysRight.png"),
+				new Image("res/buckysRight.png") };
+
+		movingUp = new Animation(walkUp, duration, false); // each animation
+															// takes array of
+															// images, duration
+															// for each image,
+															// and autoUpdate
+															// (just set to
+															// false)
+		movingDown = new Animation(walkDown, duration, false);
+		movingLeft = new Animation(walkLeft, duration, false);
+		movingRight = new Animation(walkRight, duration, false);
+		bucky = movingDown; // by default as soon as game loads, bucky will be
+							// facing down
+	}
+
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
+			throws SlickException {
+		worldMap.draw(buckyPositionX, buckyPositionY); // draw the map at 0,0 to
+														// start
+		bucky.draw(shiftX, shiftY); // draw bucky at 320, 160 (center of the
+									// screen)4
+		sprite1.draw(buckyPositionX + spritex, buckyPositionY + spritey);// first
+																			// sprite
+		// test
+
+		g.drawString("X Position: " + buckyPositionX + "\nY Position: "
+				+ buckyPositionY + "\nSpritex: " + spritex + "\nSpritey: "
+				+ spritey, 750, 20); // indicator
+		// to
+		// see
+		// where
+		// bucky
+		// is
+		// in his world
+
+		// when they press escape
+		if (quit == true) {
+			menu.draw(0, 0);
+			if (quit == false) {
+				g.clear();
+			}
+		}
+	}
+
+	public void update(GameContainer gc, StateBasedGame sbg, int delta)
+			throws SlickException {
+		Input input = gc.getInput();
+//CHANGE TO SWITCH STATEMENT
+		// sprite 1
+		if (spritex < 900) {
+			spritex += delta * .05f;
+		}
+		if (spritex > 900) {
+			//spritex = 900;
+			spritey += delta * .05;
+		}
+		if (spritey > 850) {
+			spritex -= delta * .05f;
+		}
+		if (spritex < 675){
+			spritey -= delta * .05f;
+		}
+
+		// during the game if the user hits the up arrow...
+		if (quit == false) {
+			if (input.isKeyDown(Input.KEY_UP)) {
+				bucky = movingUp; // change bucky to up image
+				buckyPositionY += delta * .1f; // increase the Y coordinates of
+												// bucky (move him up)
+				/*
+				 * if (buckyPositionY > -321) { buckyPositionY -= delta * .1f;
+				 * // dont let him keep going up // if he reaches the top }
+				 */
+			}
+			if (input.isKeyDown(Input.KEY_DOWN)) {
+				bucky = movingDown;
+				buckyPositionY -= delta * .1f;
+				/*
+				 * if (buckyPositionY < -1080) { buckyPositionY += delta * .1f;
+				 * }
+				 */
+			}
+			if (input.isKeyDown(Input.KEY_LEFT)) {
+				bucky = movingLeft;
+				buckyPositionX += delta * .1f;
+				/*
+				 * if (buckyPositionX > 99) { buckyPositionX -= delta * .1f; }
+				 */
+			}
+			if (input.isKeyDown(Input.KEY_RIGHT)) {
+				bucky = movingRight;
+				buckyPositionX -= delta * .1f;
+				/*
+				 * if (buckyPositionX < -1060) { buckyPositionX += delta * .1f;
+				 * }
+				 */
+			}
+		} else {
+		}
+
+		// escape
+		if (input.isKeyDown(Input.KEY_ESCAPE)) {
+			quit = true;
+		}
+
+		// when they hit escape
+		if (quit == true) {
+			if (input.isKeyDown(Input.KEY_R)) {
+				quit = false;
+			}
+			if (input.isKeyDown(Input.KEY_M)) {
+				quit = false;
+				sbg.enterState(0);
+				try {
+					Thread.sleep(250);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			if (input.isKeyDown(Input.KEY_Q)) {
+				System.exit(0);
+			}
+		}
+	}
+
+	public int getID() {
+		return 1;
+	}
+}
